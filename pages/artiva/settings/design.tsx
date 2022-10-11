a6a83207a25a8a3662c6ac1a524ff6847c3e97a5
neokry <patrickgenevich@gmail.com>
2022-10-11 16:03:17 -0700

    Fixes issue with saving designs

--- pages/artiva/settings/design.tsx
+++ pages/artiva/settings/design.tsx
@@ -1,7 +1,4 @@
 import AdminLayout from "@/admin/AdminLayout";
-import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { getWalletClient, WalletAppContext } from "configs/wallet-config";
-import { ReactChild } from "react";
 import DesignHeader from "@/admin/design/DesignHeader";
 import DesignerContext from "@/context/DesignerContext";
 import DesignerPreviewWrapper from "@/admin/DesignerPreviewWrapper";
@@ -12,35 +9,15 @@ import DesignerPreviewComponent, {
 const Design = () => {
   return (
     <DesignerContext.Provider>
-      <RainbowKitWrapper>
-        <AdminLayout>
-          <div className="p-6 px-10 h-full">
-            <DesignHeader />
-            <DesignerPreviewWrapper>
-              <DesignerPreviewComponent type={DesignerSitePreviewType.HOME} />
-            </DesignerPreviewWrapper>
-          </div>
-        </AdminLayout>
-      </RainbowKitWrapper>
+      <AdminLayout>
+        <div className="p-6 px-10 h-full">
+          <DesignHeader />
+          <DesignerPreviewWrapper>
+            <DesignerPreviewComponent type={DesignerSitePreviewType.HOME} />
+          </DesignerPreviewWrapper>
+        </div>
+      </AdminLayout>
     </DesignerContext.Provider>
-  );
-};
-
-const RainbowKitWrapper = ({ children }: { children: ReactChild }) => {
-  const { chains } = getWalletClient(WalletAppContext.ADMIN);
-  const { data } = DesignerContext.useContainer();
-  return (
-    <RainbowKitProvider
-      chains={chains}
-      theme={lightTheme({
-        accentColor: data?.accent_color,
-        borderRadius: "medium",
-        fontStack: "system",
-        overlayBlur: "small",
-      })}
-    >
-      {children}
-    </RainbowKitProvider>
   );
 };
 

8d54eee249051fd058b0c1b729100a2278f5e2bf
neokry <patrickgenevich@gmail.com>
2022-10-19 13:52:00 -0700

    logging

--- services/platform-graph/client.ts
+++ services/platform-graph/client.ts
@@ -1,2 +1,3 @@
 import { GraphQLClient } from "graphql-request";
+console.log("subgraph_url", process.env.NEXT_PUBLIC_ARTIVA_SUBGRAPH_URL);
 export default new GraphQLClient(process.env.NEXT_PUBLIC_ARTIVA_SUBGRAPH_URL!);

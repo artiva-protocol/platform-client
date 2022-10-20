654b6b89db16d826888d6d699f9d20cca96d19fc
neokry <patrickgenevich@gmail.com>
2022-10-19 20:26:01 -0700

    Fixes admin navigation

--- admin/AdminNavigation.tsx
+++ admin/AdminNavigation.tsx
@@ -8,6 +8,8 @@ import useSignOut from "hooks/auth/useSignOut";
 import { useRouter } from "next/router";
 import { CustomConnectButton } from "@artiva/shared";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+import Image from "next/future/image";
 
 const AdminNavigation = () => {
   const router = useRouter();
@@ -19,43 +21,45 @@ const AdminNavigation = () => {
       : "font-light text-gray-400";
   };
 
-  const push = (key: string) => {
-    router.push("/artiva/" + key, undefined);
-  };
-
   return (
     <div className="p-8 border-r h-screen relative" style={{ width: "400px" }}>
       <div className="flex items-center">
-        <img src="/artiva-logo.svg" className="w-7 mr-4" />
+        <Image
+          alt="logo"
+          src="/artiva-logo.svg"
+          className="w-7 mr-4"
+          width={20}
+          height={20}
+        />
         <div className="text-sm font-semibold text-gray-700">Neos Artworks</div>
       </div>
       <div className="mt-10">
-        <button onClick={() => push("site")} className="flex items-center mt-4">
-          <HomeIcon className="mr-4 w-5 text-gray-400" />
-          <div className={`text-sm text-gray-600 ${getStyle("site")}`}>
-            View Site
-          </div>
-        </button>
+        <Link href={"/artiva/site"}>
+          <a className="flex items-center mt-4">
+            <HomeIcon className="mr-4 w-5 text-gray-400" />
+            <div className={`text-sm text-gray-600 ${getStyle("site")}`}>
+              View Site
+            </div>
+          </a>
+        </Link>
 
-        <button
-          onClick={() => push("collection")}
-          className="flex items-center mt-4"
-        >
-          <RectangleStackIcon className="mr-4 w-5 text-gray-400" />
-          <div className={`text-sm text-gray-600 ${getStyle("collection")}`}>
-            Collection
-          </div>
-        </button>
+        <Link href={"/artiva/collection"}>
+          <a className="flex items-center mt-4">
+            <RectangleStackIcon className="mr-4 w-5 text-gray-400" />
+            <div className={`text-sm text-gray-600 ${getStyle("collection")}`}>
+              Collection
+            </div>
+          </a>
+        </Link>
 
-        <button
-          onClick={() => push("settings")}
-          className="flex items-center mt-4"
-        >
-          <CogIcon className="mr-4 w-5 text-gray-400" />
-          <div className={`text-sm text-gray-600 ${getStyle("settings")}`}>
-            Settings
-          </div>
-        </button>
+        <Link href={"/artiva/settings"}>
+          <a className="flex items-center mt-4">
+            <CogIcon className="mr-4 w-5 text-gray-400" />
+            <div className={`text-sm text-gray-600 ${getStyle("settings")}`}>
+              Settings
+            </div>
+          </a>
+        </Link>
 
         <button onClick={signOut} className="flex items-center mt-8">
           <LockClosedIcon className="mr-4 w-5 text-gray-400 " />

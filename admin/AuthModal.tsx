6ec392aecfbacba3534a28572f0f106ad7bd8d61
neokry <patrickgenevich@gmail.com>
2022-11-01 13:10:05 -0700

    Fix admin modal connection issue

--- admin/AuthModal.tsx
+++ admin/AuthModal.tsx
@@ -1,9 +1,10 @@
 import ArtivaConnectButton from "@/components/ArtivaConnectButton";
 import Image from "next/future/image";
 import React from "react";
-import { useNetwork, useSwitchNetwork } from "wagmi";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
 
 const AuthModal = () => {
+  const { address } = useAccount();
   const { chain } = useNetwork();
   const { switchNetwork } = useSwitchNetwork();
 
@@ -14,7 +15,7 @@ const AuthModal = () => {
         <div className="text-4xl font-bold mt-10">Welcome to Artiva</div>
         <div className="text-gray-400">Connect your wallet to get started</div>
 
-        {chain?.id == process.env.NEXT_PUBLIC_PROTOCOL_NETWORK ? (
+        {!address || chain?.id == process.env.NEXT_PUBLIC_PROTOCOL_NETWORK ? (
           <ArtivaConnectButton
             className={`bg-black text-white w-72 rounded-md h-8 text-sm focus:outline-none mt-8`}
           />

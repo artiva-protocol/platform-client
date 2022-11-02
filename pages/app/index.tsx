724e00972fee64bdd2ed972803865e1f19329637
neokry <patrickgenevich@gmail.com>
2022-11-02 14:12:55 -0700

    Adds skeleton loader to app

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -25,34 +25,55 @@ const AppIndex = () => {
   );
 
   return (
-    <div className=" bg-gray-100 min-h-screen pb-10">
+    <div className="min-h-screen pb-10">
       <AppHeader />
-      {status !== "loading" && (
-        <Fragment>
-          {(!address || data?.length == 0) && (
-            <div className="flex items-center justify-around w-full h-[80vh]">
-              <div className="flex flex-col items-center">
-                <div className="text-2xl font-semibold">No platforms found</div>
-                <Link href="/create">
-                  <a className="bg-black flex items-center justify-around text-white h-8 w-[20vw] rounded-md mt-4">
-                    Create my Platform
+      <div className="border-t">
+        {status !== "loading" ? (
+          <Fragment>
+            {(!address || data?.length == 0) && (
+              <div className="flex items-center justify-around w-full h-[80vh]">
+                <div className="flex flex-col items-center">
+                  <div className="text-2xl font-semibold">
+                    No platforms found
+                  </div>
+                  <Link href="/create">
+                    <a className="bg-black flex items-center justify-around text-white h-8 w-[20vw] rounded-md mt-4">
+                      Create my Platform
+                    </a>
+                  </Link>
+                  <ArtivaConnectButton className="border border-gray-500 flex items-center justify-around h-8 w-[20vw] rounded-md mt-2" />
+                </div>
+              </div>
+            )}
+            <div className="grid grid-cols-3 gap-6 px-6 pt-6">
+              {data?.map((x) => (
+                <Link href={`platform/${x.contract}`} key={x.contract}>
+                  <a className="h-[35vh]">
+                    <PlatformPlacard platform={x} />
                   </a>
                 </Link>
-                <ArtivaConnectButton className="border border-gray-500 flex items-center justify-around h-8 w-[20vw] rounded-md mt-2" />
-              </div>
+              ))}
             </div>
-          )}
-          <div className="grid grid-cols-3 gap-6 px-6 pt-6">
-            {data?.map((x) => (
-              <Link href={`platform/${x.contract}`} key={x.contract}>
-                <a className="h-[35vh]">
-                  <PlatformPlacard platform={x} />
-                </a>
-              </Link>
-            ))}
-          </div>
-        </Fragment>
-      )}
+          </Fragment>
+        ) : (
+          <PlatformSkeleton />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const PlatformSkeleton = () => {
+  return (
+    <div className="grid grid-cols-3 gap-6 px-6 pt-6">
+      {Array(6)
+        .fill("")
+        .map((_, i) => (
+          <div
+            key={i}
+            className="relative w-full h-[33vh] text-left animate-pulse rounded-md bg-gray-200"
+          />
+        ))}
     </div>
   );
 };

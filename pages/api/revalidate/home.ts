4514aad5763f80628faef88cabde88632466c813
neokry <patrickgenevich@gmail.com>
2022-11-04 12:42:21 -0700

    Fix revalidation

--- pages/api/revalidate/home.ts
+++ pages/api/revalidate/home.ts
@@ -1,11 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   try {
-    await res.revalidate(`/_platforms/${req.query.platform}`);
+    await res.revalidate(`/`);
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");

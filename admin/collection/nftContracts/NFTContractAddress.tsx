d9215abc546ae78f005ee328bde69d13abb7501a
neokry <patrickgenevich@gmail.com>
2022-11-18 11:51:13 -0800

    Adds validation to address curator

--- admin/collection/nftContracts/NFTContractAddress.tsx
+++ admin/collection/nftContracts/NFTContractAddress.tsx
@@ -1,9 +1,11 @@
 import NFTContractContext from "@/context/NFTContractContext";
+import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
 const NFTContractAddress = () => {
   const { setAddress: setContextAddress } = NFTContractContext.useContainer();
   const [address, setAddress] = useState<string>("");
+  const [error, setError] = useState("");
 
   const onAddressChange = (e: any) => {
     setAddress(e.target.value);
@@ -11,6 +13,14 @@ const NFTContractAddress = () => {
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
+      if (!address) return;
+      if (!ethers.utils.isAddress(address)) {
+        setError("Error: invalid address");
+        return;
+      }
+
+      setError("");
+
       setContextAddress(address);
     }, 1000);
 
@@ -22,9 +32,11 @@ const NFTContractAddress = () => {
       <div>Contract address</div>
       <input
         className="w-full rounded-md p-2 text-xs mt-1 focus:outline-none"
+        placeholder="0x765F924..."
         value={address}
         onChange={onAddressChange}
       />
+      {error && <p className="text-red-500 mt-2">{error}</p>}
     </div>
   );
 };

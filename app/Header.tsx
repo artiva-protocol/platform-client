06f1242a39a98dba88df77a85a435bbc2803ec61
neokry <patrickgenevich@gmail.com>
2022-11-20 13:01:36 -0800

    Fixes app links

--- app/Header.tsx
+++ app/Header.tsx
@@ -8,23 +8,21 @@ const AppHeader = () => {
   return (
     <Fragment>
       <div className="flex justify-between items-center h-20 px-8 bg-white">
-        <Link href="/">
-          <a className="flex items-center">
-            <Image
-              src={"/text-logo.svg"}
-              className="h-full"
-              alt="logo"
-              width="120"
-              height="50"
-            />
-          </a>
+        <Link href="/" className="flex items-center">
+
+          <Image
+            src={"/text-logo.svg"}
+            className="h-full"
+            alt="logo"
+            width="120"
+            height="50"
+          />
+
         </Link>
         <div className="flex items-center">
-          <Link href={"/create"}>
-            <a className="flex items-center mr-8 text-gray-800">
-              <PlusIcon className="h-4 mr-2 text-gray-800" />
-              Create
-            </a>
+          <Link href={"/create"} className="flex items-center mr-8 text-gray-800">
+
+            <PlusIcon className="h-4 mr-2 text-gray-800" />Create
           </Link>
           <ArtivaConnectButton className="flex item-center justify-around border border-gray-400 text-gray-500 w-40 rounded-md text-center" />
         </div>
